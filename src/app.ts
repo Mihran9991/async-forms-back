a119697930eda465266cdac824fb312faa99496b
mihran9991 <mihran@simplytechnologies.net>
2019-12-09 22:13:10 +0400

    feat(socketIO): integrated socketIO

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,18 @@
 // lib/app.ts
 import express from 'express';
+import SocketIO from "./services/socketIo";
 
-// Create a new express application instance
-const app: express.Application = express();
+class App {
+  private app: express.Application = express();
+  private server: any = this.app.listen(3000);
 
-app.listen(3000, () => {
-  console.log("App listening on port 3000!");
-});
+  public async start() {
+    console.log("App listening on port 3000!");
+    await SocketIO.getInstance().connect(this.server);
+    console.log("SocketIO is connected to the app!");
+  }
+}
+
+new App().start();
+
+
